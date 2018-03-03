06da80fa9dda7c4d0811ae0f21c0bccb42637248
Jesús Arias <jesusariasgil1995@hotmail.com>
2018-03-03 22:02:00 +0100

    Modificados los ficheros

--- ejercicio.py
+++ ejercicio.py
@@ -1,4 +1,4 @@
-from funciones import clasificacion
+from funciones import clasificacion, equiposgolesfavor, rangopuntos, competicion
 import urllib.request, json 
 
 with urllib.request.urlopen("http://apiclient.resultados-futbol.com/scripts/api/api.php?key=7171673be04cc06aa2426307d8b42836&tz=Europe/Madrid&format=json&req=tables&league=1&group=1") as url:
@@ -6,3 +6,15 @@ with urllib.request.urlopen("http://apiclient.resultados-futbol.com/scripts/api/
 
 for i in clasificacion(datos):
 	print(i[0], i[1])
+
+for n in equiposgolesfavor(datos):
+	print(n[0], "GF:", n[1], "GC:", n[2])
+
+puntosmin=input("Dime los puntos minimos: ")
+puntosmax=input("Dime los puntos maximos: ")
+for r in rangopuntos(datos, puntosmin, puntosmax):
+	print(r[0], r[1], r[2])
+
+compe=input("Dime alguna de estas opciones (Champions League / Europa League / Descenso): ")
+for c in competicion(datos, compe):
+	print(c[0], c[1], c[2])
--- funciones.py
+++ funciones.py
@@ -1,6 +1,30 @@
 def clasificacion(dict):
 	clas=[]
 	for i in dict.get("table"):
-		clas.append([i.get("team"), i.get("points")])
+		clas.append([i.get("pos"), i.get("team"), i.get("points")])
 	return clas
 
+def equiposgolesfavor(dict):
+	equipos=[]
+	for i in dict.get("table"):
+		if i.get("gf")>i.get("ga"):
+			equipos.append([i.get("team"), i.get("gf"), i.get("ga")])
+	return equipos
+
+def rangopuntos(dict, puntosmin, puntosmay):
+	rango=[]
+	for i in dict.get("table"):
+		if i.get("points")>=puntosmin and i.get("points")<=puntosmay:
+			rango.append([i.get("pos"), i.get("team"), i.get("points")])
+	return rango
+
+def competicion(dict, cat):
+	comp=[]
+	for i in dict.get("table"):
+		if cat=="Champions League" and i.get("mark")==1:
+			comp.append([i.get("pos"), i.get("team"), i.get("points")])
+		elif cat=="Europa League" and i.get("mark")==2:
+			comp.append([i.get("pos"), i.get("team"), i.get("points")])
+		elif cat=="Descenso" and i.get("mark")==3:
+			comp.append([i.get("pos"), i.get("team"), i.get("points")])
+	return comp
